af8719d1b09a69acac0ba05d9d250ff3b0cdd642
Markeesmo <76288157+hybridmarko720@users.noreply.github.com>
2022-08-04 00:16:36 -0500

    Create removeStringSpaces.js
    Add Remove String Spaces

---
+++ 8kyu/removeStringSpaces.js
@@ -0,0 +1,7 @@
+// Simple, remove the spaces from the string, then return the resultant string.
+
+// My Solution:
+function noSpace(x){
+    let remWhiteSpace = x.replace(/\s/g, "")
+    return remWhiteSpace
+  }
